827c6338017adc255684fb7b745f85347de1c6a0
guivictorr <guilhermevictor20@gmail.com>
2021-06-16 22:25:09 -0300

    feat(page): fetching 150 pokemons at seeAll

--- src/pages/dashboard/seeAll.tsx
+++ src/pages/dashboard/seeAll.tsx
@@ -1,12 +1,55 @@
+import { useState, useCallback, useEffect } from 'react';
+
+import fetchJson from '../../utils/fetchJson';
+import PokemonProps from '../../@types/pokemon';
+
+import Grid from '../../components/Grid/styles';
 import Layout from '../../components/Layout';
+import Card from '../../components/Card';
 
 import * as S from '../../styles/pages/seeAll';
+import Heading from '../../components/Heading/styles';
+
+type ApiResult = {
+  results: {
+    name: string;
+    url: string;
+  }[];
+};
 
 const DashBoard = () => {
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=200';
+  const [pokemons, setPokemons] = useState<PokemonProps[]>([]);
+
+  const getData = useCallback(async () => {
+    const { results } = await fetchJson<ApiResult>(url);
+
+    for (const result of results) {
+      const pokemon = await fetchJson<PokemonProps>(result.url);
+      setPokemons(prevState => [...prevState, pokemon]);
+    }
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
   return (
     <Layout>
       <S.Container>
-        <h1>seeAll</h1>
+        {pokemons.length < 20 ? (
+          <S.Content>
+            <Heading level={2} color="grey500" fontSize="xlarge">
+              Loading {pokemons.length} pokemons...
+            </Heading>
+          </S.Content>
+        ) : (
+          <Grid min="200px">
+            {pokemons.map(pokemon => (
+              <Card key={pokemon.id} pokemon={pokemon} />
+            ))}
+          </Grid>
+        )}
       </S.Container>
     </Layout>
   );
--- src/styles/pages/seeAll.ts
+++ src/styles/pages/seeAll.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 20px 100px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+`;
