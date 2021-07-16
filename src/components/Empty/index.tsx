b6d8730e671eda96f779dfa320c3ede18221cce3
guivictorr <guilhermevictor20@gmail.com>
2021-07-16 20:14:02 -0300

    hotfix/empty-component

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -5,29 +5,26 @@ import Button from '../Button';
 import Heading from '../Heading/styles';
 
 import { Container } from './styles';
-import Statistic from '../Statistic';
 
 const Empty = () => {
   return (
-    <>
-      <Container>
-        <Image
-          width={600}
-          height={450}
-          src="/empty.svg"
-          alt="Astronauta em um espaço vazio"
-        />
-        <Heading level={2} fontWeight={600} fontSize="large" color="grey500">
-          Está meio vazio por aqui!
-        </Heading>
-        <p>
-          Procure por pokémons para adicioná-los aos seus <br /> favoritos.
-        </p>
-        <Link href="/dashboard/search">
-          <Button title="Procurar pokémons" outlined />
-        </Link>
-      </Container>
-    </>
+    <Container>
+      <Image
+        width={600}
+        height={450}
+        src="/empty.svg"
+        alt="Astronauta em um espaço vazio"
+      />
+      <Heading level={2} fontWeight={600} fontSize="large" color="grey500">
+        Está meio vazio por aqui!
+      </Heading>
+      <p>
+        Procure por pokémons para adicioná-los aos seus <br /> favoritos.
+      </p>
+      <Link href="/dashboard/search">
+        <Button title="Procurar pokémons" outlined />
+      </Link>
+    </Container>
   );
 };
 
