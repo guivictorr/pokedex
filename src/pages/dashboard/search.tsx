a157f5d03be5d822ed4034013300c5de9eba4969
guivictorr <guilhermevictor20@gmail.com>
2021-08-09 21:34:17 -0300

    feat(page): search page

---
+++ src/pages/dashboard/search.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent, useState } from 'react';
+import PokemonProps from '../../@types/pokemon';
+import Card from '../../components/Card';
+import Grid from '../../components/Grid/styles';
+import Input from '../../components/Input';
+import Layout from '../../components/Layout';
+import useDebounce from '../../hooks/useDebounce';
+import useFetch from '../../hooks/useFetch';
+
+import * as S from '../../styles/pages/seeAll';
+
+const Search = () => {
+  const [inputText, setInputText] = useState<string>('');
+  const debouncedValue = useDebounce(inputText);
+  const url = `https://pokeapi.co/api/v2/pokemon/${debouncedValue}`;
+  const { result } = useFetch<PokemonProps>(url);
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value || '');
+  };
+
+  return (
+    <Layout>
+      <S.Container>
+        <Input placeholder="Procure pelo nome..." onChange={handleInput} />
+        <Grid min="250px">
+          {result && !('count' in result) && <Card {...result} />}
+        </Grid>
+      </S.Container>
+    </Layout>
+  );
+};
+
+export default Search;
