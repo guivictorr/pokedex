263bb584fd2a06dcb6d21c3e606e6915eddb922b
guivictorr <guilhermevictor20@gmail.com>
2021-09-26 02:03:05 -0300

    feat(statistics): tests for statistics component

--- src/components/Statistic/index.tsx
+++ src/components/Statistic/index.tsx
@@ -2,10 +2,10 @@ import * as S from './styles';
 
 type StatisticProps = {
   percentage: number;
-  label: string;
+  label: StatKeys;
 };
 
-enum StatNames {
+export enum StatNames {
   hp = 'HP',
   attack = 'ATK',
   defense = 'DEF',
@@ -14,14 +14,15 @@ enum StatNames {
   speed = 'SPD',
 }
 
-type StatKeys = keyof typeof StatNames;
+export type StatKeys = keyof typeof StatNames;
 
 const Statistic = ({ percentage, label }: StatisticProps) => {
   return (
     <S.Container>
-      <p>{StatNames[label as StatKeys]}</p>
+      <p>{StatNames[label]}</p>
       <S.ProgressBar>
         <S.Filler
+          data-testid="filler"
           initial={{ width: 0 }}
           animate={{ width: percentage > 100 ? '100%' : percentage + '%' }}
           transition={{ duration: 1 }}
